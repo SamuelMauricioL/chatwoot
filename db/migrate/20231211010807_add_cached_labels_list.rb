19893463c8a8414d882b18ec76abe090663e096b
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2026-07-25 09:30:22 -0500

    fix: skip broken ActsAsTaggableOn migration in v4.16.1

--- db/migrate/20231211010807_add_cached_labels_list.rb
+++ db/migrate/20231211010807_add_cached_labels_list.rb
@@ -2,6 +2,7 @@ class AddCachedLabelsList < ActiveRecord::Migration[7.0]
   def change
     add_column :conversations, :cached_label_list, :string
     Conversation.reset_column_information
-    ActsAsTaggableOn::Taggable::Cache.included(Conversation)
+    # Fix: ActsAsTaggableOn::Taggable::Cache no longer exists in newer gem versions
+    # ActsAsTaggableOn::Taggable::Cache.included(Conversation)
   end
 end
